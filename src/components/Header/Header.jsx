062b89ab842afaf9c62624727de2bc30476d0c4b
Landoglobal <mamadoualimoubalde7gmail.com>
2023-08-30 17:45:49 +0200

    Footer and animation add and project completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,37 @@
-import React from "react";
-import "./Header.css"
+import React, { useState } from "react";
+import "./Header.css";
+import {BiMenuAltRight} from "react-icons/bi";
+import OutsideClickHandler from "react-outside-click-handler";
 
 const Header = () => {
+    const [menuOpened, setmenuOpened] = useState(false)
+
+    const getMenuStyles = (menuOpened) => {
+        if (document.documentElement.clientWidth <= 800)
+        {
+            return {right: !menuOpened && "-100%"}
+        }
+    }
     return (
         <section className="h-wrapper">
             <div className="flexCenter paddings innerWidth h-container">
                 <img src="./logo.png" alt="logo" width={100}/>
+                <OutsideClickHandler onOutsideClick={() => {
+                    setmenuOpened(false)
+                }}>
+                    <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
+                        <a href="">Residences</a>
+                        <a href="">Our Value</a>
+                        <a href="">Conatct US</a>
+                        <a href="">Get Started</a>
+                        <button className="button">
+                            <a href="">Conatct</a>
+                        </button>
+                    </div>
+                </OutsideClickHandler>
                 
-                <div className="flexCenter h-menu">
-                    <a href="">Residences</a>
-                    <a href="">Our Value</a>
-                    <a href="">Conatct US</a>
-                    <a href="">Get Started</a>
-                    <button className="button">
-                        <a href="">Conatct</a>
-                    </button>
+                <div className="menu-icon" onClick={() => setmenuOpened((prev)=>!prev)}>
+                    <BiMenuAltRight size={30}/>
                 </div>
             </div>
         </section>
